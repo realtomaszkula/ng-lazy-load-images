dad8a362dfaa7f38fa9ad37eb1d542243c1b8d0a
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-06-03 18:31:30 +0200

    feat: add lazy-load directive

---
+++ src/app/image-lazy-load/lazy-load.directive.ts
@@ -0,0 +1,36 @@
+import { AfterViewInit, Directive, ElementRef, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: 'img[appLazyLoad]'
+})
+export class LazyLoadDirective implements AfterViewInit {
+  @HostBinding('attr.src') srcAttr = null;
+  @Input() src: string;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.canLazyLoad() ? this.lazyLoadImage() : this.loadImage();
+  }
+
+  private canLazyLoad() {
+    return window && 'IntersectionObserver' in window;
+  }
+
+  private lazyLoadImage() {
+    const obs = new IntersectionObserver(entries => {
+      entries.forEach(({ isIntersecting }) => {
+        if (isIntersecting) {
+          this.loadImage();
+          obs.unobserve(this.el.nativeElement);
+        }
+      });
+    });
+    obs.observe(this.el.nativeElement);
+  }
+
+  private loadImage() {
+    this.srcAttr = this.src;
+  }
+
+}
